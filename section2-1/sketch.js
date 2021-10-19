6b021af6e0370d76e6669e7028f426bb4611c093
nitamago1 <uenohagc@gmail.com>
2021-10-19 18:17:04 +0900

    section2-1

--- section2-1/sketch.js
+++ section2-1/sketch.js
@@ -9,6 +9,13 @@ function setup() {
   }
 }
 
+ function setup(){
+   createCanvas(100,100);
+   fill(0);
+   for(let i = 0; i < 5; i++){
+     rect(i * 20, i * 20, 20, 20);
+   }
+ }
 // for 文を使うと、同じプログラムを以下のように書くことができます
 // function setup() {
 //   createCanvas(100,100);
