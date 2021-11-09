6f1c071396d1153e77eca8569199f67a77b96e64
nitamago1 <uenohagc@gmail.com>
2021-11-09 10:36:28 +0900

    section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -11,7 +11,7 @@ function setup(){
 function draw(){
   background(160, 192, 255);
 
-  if(KeyIsDown(...)){
+  if(KeyIsDown("".charCodeAt(0))){
     count = (count + 1) % cycle;
   }
   else{
@@ -20,10 +20,10 @@ function draw(){
   // BLANK[1]
 
   if(count < 50){
-    ellipse(width / 2, height / 2, count );
+    ellipse(width / 2, height / 2, count);
   }
   else {
-    ellipse(width / 2, height / 2, 100 - count );
+    ellipse(width / 2, height / 2, 100 - count);
   }
 
   if(mouseIsPressed){
@@ -34,7 +34,7 @@ function draw(){
   //background(160, 192, 255);
   //for(let i = 0; i < 10; i++){
   //ellipse(width * i / 10, height * (10 - i) / 10, 20);
-}
+//}
 //text("center!", width * 0.5, height * 0.5);
 }
 
@@ -50,7 +50,7 @@ function draw(){
   //y = height / 2;
   //vx = 8;
   //vy = 8;
-}
+//}
 
 //function draw(){
   //background(160, 192, 255);
@@ -72,8 +72,8 @@ function draw(){
   //if(y > height){ vy = -1 * vy; }
   //x = constrain(x, 0, width);
   //y = constrain(y, 0, height);
-}
+//}
 
 //function windowResized(){
 //  resizeCanvas(windowWidth, windowHeight);
-}
+//}
